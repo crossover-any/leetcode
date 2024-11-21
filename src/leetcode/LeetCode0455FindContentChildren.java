a744c3f780efb2ca8597d39e650650abb2090d5c
Tariq <tengxingqiang@126.com>
2024-11-21 11:16:25 +0800

    feat: 0455分发饼干-贪心

---
+++ src/leetcode/LeetCode0455FindContentChildren.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @author tengxq
+ */
+public class LeetCode0455FindContentChildren {
+    public int findContentChildren(int[] g, int[] s) {
+        int ctn = 0;
+        int gIndex = 0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        for (int i = 0; i < s.length && gIndex < g.length; i++) {
+            if (g[gIndex] <= s[i]) {
+                ctn++;
+                gIndex++;
+            }
+        }
+        return ctn;
+    }
+}
