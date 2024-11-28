331ca3f38e99753945c9f69a9a0f84c5fb68eef1
Tariq <tengxingqiang@126.com>
2024-11-28 15:25:34 +0800

    feat: 02020有效的括号

---
+++ src/leetcode/group/stack/LeetCode0020IsValid.java
@@ -0,0 +1,38 @@
+package leetcode.group.stack;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Stack;
+
+/**
+ *
+ *
+ * @author tengxq
+ */
+public class LeetCode0020IsValid {
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        char[] charArray = s.toCharArray();
+        for (char c : charArray) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if (c == ')' && top != '(') {
+                    return false;
+                }
+                if (c == ']' && top != '[') {
+                    return false;
+                }
+                if (c == '}' && top != '{') {
+                    return false;
+                }
+            }
+        }
+        return stack.empty();
+    }
+}
