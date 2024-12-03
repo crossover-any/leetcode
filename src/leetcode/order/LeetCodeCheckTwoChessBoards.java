644c0f87033f0b647f3f1549c4cc8fcd6d9faed4
Tariq <tengxingqiang@126.com>
2024-12-03 15:16:48 +0800

    feat: 3274检查棋盘方格颜色是否相同

---
+++ src/leetcode/order/LeetCodeCheckTwoChessBoards.java
@@ -0,0 +1,19 @@
+package leetcode.order;
+
+/**
+ * @author tengxq
+ */
+public class LeetCodeCheckTwoChessBoards {
+
+    public static void main(String[] args) {
+        LeetCodeCheckTwoChessBoards test = new LeetCodeCheckTwoChessBoards();
+        System.out.println(test.checkTwoChessboards("a1", "b1"));
+    }
+
+    private static final int[] chessboards = new int[]{0b10101010, 0b01010101};
+
+    public boolean checkTwoChessboards(String coordinate1, String coordinate2) {
+        int chessboard = (chessboards[(coordinate1.charAt(0) - 'a')%2]<<(coordinate1.charAt(1) - '1')) & 0b10000000 ^ (chessboards[(coordinate2.charAt(0) - 'a')%2]<<(coordinate2.charAt(1) - '1')) & 0b10000000;
+        return chessboard == 0;
+    }
+}
