d4dcb70f366ff14607447bff1d858b40cc1c58d0
Tariq <tengxingqiang@126.com>
2024-12-05 10:37:13 +0800

    feat: 0222完全二叉树的节点个数

---
+++ src/leetcode/order/LeetCode0222CountNodes.java
@@ -0,0 +1,53 @@
+package leetcode.order;
+
+import model.TreeNode;
+
+/**
+ * @author tengxq
+ */
+public class LeetCode0222CountNodes {
+
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int level = 0;
+        TreeNode node = root;
+        while (node.left != null) {
+            level++;
+            node = node.left;
+        }
+        int low = 1 << level, high = (1 << (level + 1)) - 1;
+        while (low < high) {
+            int mid = (high - low + 1) / 2 + low;
+            if (exists(root, level, mid)) {
+                low = mid;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
+    /**
+     * 判断是否存在第k个节点
+     *
+     * @param root  根节点
+     * @param level 层高    >=1
+     * @param k     节点编号 >=1
+     * @return
+     */
+    public boolean exists(TreeNode root, int level, int k) {
+        int bits = 1 << (level - 1);
+        TreeNode node = root;
+        while (node != null && bits > 0) {
+            if ((bits & k) == 0) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+            bits >>= 1;
+        }
+        return node != null;
+    }
+}
