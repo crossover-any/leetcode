0d8a0ac0cfc7431587559c0642cc90f5a7781d58
Tariq <tengxingqiang@126.com>
2024-12-06 14:44:56 +0800

    feat: 0257二叉树的所有路径

---
+++ src/leetcode/group/tree/LeetCode0257BInaryTreePaths.java
@@ -0,0 +1,32 @@
+package leetcode.group.tree;
+
+import model.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author tengxq
+ */
+public class LeetCode0257BInaryTreePaths {
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> list = new ArrayList<>();
+        dfs(root, "->", list);
+        return list;
+    }
+
+    public void dfs(TreeNode node, String path, List<String> list) {
+        if (node.left == null && node.right == null) {
+            path += node.val;
+            list.add(path.replaceFirst("->", ""));
+        }
+        path  = path + node.val + "->";
+        if (node.left != null) {
+            dfs(node.left, path , list);
+        }
+        if (node.right != null) {
+            dfs(node.right, path, list);
+        }
+    }
+}
