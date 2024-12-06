2b9003afe943cceffc052ce5a8ac1a13dc3ef8c2
Tariq <tengxingqiang@126.com>
2024-12-06 14:56:23 +0800

    feat: 0404左叶子之和

---
+++ src/leetcode/group/tree/LeetCode0404SumOfLeftLeaves.java
@@ -0,0 +1,21 @@
+package leetcode.group.tree;
+
+import model.TreeNode;
+
+/**
+ * @author tengxq
+ */
+public class LeetCode0404SumOfLeftLeaves {
+
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            return root.left.val + sumOfLeftLeaves(root.right);
+        }
+        return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+    }
+
+
+}
