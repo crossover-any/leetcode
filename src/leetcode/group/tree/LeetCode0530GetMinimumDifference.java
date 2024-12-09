7fed2ea0bf2f0ec00ddb7bd9643e34fb3000fcbb
Tariq <tengxingqiang@126.com>
2024-12-09 11:07:17 +0800

    feat: 0530二叉搜索树的最小绝对值

---
+++ src/leetcode/group/tree/LeetCode0530GetMinimumDifference.java
@@ -0,0 +1,30 @@
+package leetcode.group.tree;
+
+import model.TreeNode;
+
+/**
+ * @author tengxq
+ */
+public class LeetCode0530GetMinimumDifference {
+
+    private Integer pre = null;
+
+    private int minAbs = Integer.MAX_VALUE;
+
+    public int getMinimumDifference(TreeNode root) {
+        traverse(root);
+        return minAbs;
+    }
+
+    public void traverse(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        traverse(root.left);
+        if (pre != null) {
+            minAbs = Math.min(minAbs, root.val - pre);
+        }
+        pre = root.val;
+        traverse(root.right);
+    }
+}
