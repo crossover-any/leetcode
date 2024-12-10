0e7013ca5e5309e270c25cdb8b735ef3648265d5
Tariq <tengxingqiang@126.com>
2024-12-10 15:07:33 +0800

    feat: 1038从二叉搜索树到更大和数

---
+++ src/leetcode/group/tree/LeetCode1038BstToGst.java
@@ -0,0 +1,22 @@
+package leetcode.group.tree;
+
+import model.TreeNode;
+
+/**
+ * @author tengxq
+ */
+public class LeetCode1038BstToGst {
+
+    int sum = 0;
+
+    public TreeNode bstToGst(TreeNode root) {
+        if (root != null) {
+            bstToGst(root.right);
+            sum += root.val;
+            root.val = sum;
+            bstToGst(root.left);
+        }
+        return root;
+    }
+
+}
