b8eb6cadacc6b613f035c2d7ff7118084fb7dec2
Tariq <tengxingqiang@126.com>
2024-12-10 10:09:50 +0800

    feat: 0701二叉搜索树中的插入操作

---
+++ src/leetcode/group/tree/LeetCode0701InsertIntoBST.java
@@ -0,0 +1,34 @@
+package leetcode.group.tree;
+
+import model.TreeNode;
+
+/**
+ * @author tengxq
+ */
+public class LeetCode0701InsertIntoBST {
+
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        TreeNode p = root;
+        while (p != null) {
+            if (p.val > val) {
+                if (p.left == null) {
+                    p.left = new TreeNode(val);
+                    break;
+                } else {
+                    p = p.left;
+                }
+            } else {
+                if (p.right == null) {
+                    p.right = new TreeNode(val);
+                    break;
+                } else {
+                    p = p.right;
+                }
+            }
+        }
+        return root;
+    }
+}
