a3d147123fe4b77fd4e7843f4b97cd2ce9c6048d
Tariq <tengxingqiang@126.com>
2024-12-10 14:44:07 +0800

    feat: 0669修剪二叉搜索数

---
+++ src/leetcode/group/tree/LeetCode0669TrimBST.java
@@ -0,0 +1,24 @@
+package leetcode.group.tree;
+
+import model.TreeNode;
+
+/**
+ * @author tengxq
+ */
+public class LeetCode0669TrimBST {
+
+    public TreeNode trimBST(TreeNode root, int L, int R) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val < L) {
+            return trimBST(root.right, L, R);
+        }
+        if (root.val > R) {
+            return trimBST(root.left, L, R);
+        }
+        root.left = trimBST(root.left, L, R);
+        root.right = trimBST(root.right, L, R);
+        return root;
+    }
+}
