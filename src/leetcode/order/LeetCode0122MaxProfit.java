5a343aae2def85acba5f3c23c12c535d5b9c01b4
Tariq <tengxingqiang@126.com>
2024-12-16 11:21:55 +0800

    feat: 0122买卖股票的最佳时机II[贪心]

---
+++ src/leetcode/order/LeetCode0122MaxProfit.java
@@ -0,0 +1,16 @@
+package leetcode.order;
+
+/**
+ * @author tengxq
+ */
+public class LeetCode0122MaxProfit {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i-1]) {
+                profit += prices[i] - prices[-1];
+            }
+        }
+        return profit;
+    }
+}
