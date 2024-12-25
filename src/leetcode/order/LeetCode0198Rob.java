40db1c5f3aa519637932d30858ce0bd7ac99b7f9
Tariq <tengxingqiang@126.com>
2024-12-25 09:44:57 +0800

    feat: 0198打家劫舍[动态规划]

---
+++ src/leetcode/order/LeetCode0198Rob.java
@@ -0,0 +1,21 @@
+package leetcode.order;
+
+/**
+ * @author tengxq
+ */
+public class LeetCode0198Rob {
+
+    public int rob(int[] nums) {
+        int[] dp = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0) {
+                dp[i] = nums[i];
+            } else if (i == 1) {
+                dp[i] = Math.max(nums[i], nums[i - 1]);
+            } else {
+                dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+            }
+        }
+        return dp[nums.length - 1];
+    }
+}
